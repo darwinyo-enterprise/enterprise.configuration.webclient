e4833d8bc5d412f1140fb8c1f978c64940a03cb6
Darwin Yo <darwinyo.enterprise@outlook.com>
2017-12-24 21:50:56 +0700

    Implementation CI, Service Workers, SSR

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    // Pre-rendering Setting
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    // Server Workers
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
