5294945f3cf7cda3f9a50a58a172cd707820b5da
Darwin Yo <darwinyo.enterprise@outlook.com>
2017-12-25 19:04:10 +0700

    disable end to end test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('enterprise.configuration.webclient App', () => {
+xdescribe('enterprise.configuration.webclient App', () => {
   let page: AppPage;
 
   beforeEach(() => {
